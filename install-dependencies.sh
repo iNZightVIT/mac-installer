5e9a77116bd8afeb3270257d5c06c0f604a95784
Tom Elliott <tomelliottnz@gmail.com>
2019-01-25 23:57:02 +1300

    I think we can trust the gtk installer... :crossed_fingers:

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,7 +2,7 @@
 
 # install gtk
 wget http://r.research.att.com/libs/GTK_2.24.17-X11.pkg -O gtk.pkg
-sudo installer -pkg gtk.pkg -target /
+sudo installer -pkg gtk.pkg -target / -allowUntrusted
 
 # install xquartz
 # 
