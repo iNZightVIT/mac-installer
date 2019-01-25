5a51df43ae6b2dfde56dc90dd0df95c0d5fbb537
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 00:16:00 +1300

    And install those packages...

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,11 +5,12 @@ R_VERSION=latest
 
 # install gtk
 curl -L http://r.research.att.com/libs/GTK_2.24.17-X11.pkg -o gtk.pkg
-# sudo installer -pkg gtk.pkg -target / -allowUntrusted
+sudo installer -pkg gtk.pkg -target / -allowUntrusted
 
 # install xquartz
 curl -L https://dl.bintray.com/xquartz/downloads/XQuartz-$XQUARTZ_VERSION.dmg -o xquartz.dmg
-# hdiutil attach xquartz.dmg
+hdiutil attach xquartz.dmg
+sudo installer -pkg /Volumes/XQuartz-$XQUARTZ_VERSION/XQuartz.pkg -target / -allowUntrusted
 
 
 # install r
