0bfc38c84fa35fd428351473ed59d0d1f09ae840
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 00:01:11 +1300

    I. Missed. A. Plus.

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 set -ev
 
 ## check that GTK is installed ... 
-if [ ! -d /Library/Frameworks/GTK.framework  ]; then exit 1; fi
+if [ ! -d /Library/Frameworks/GTK+.framework  ]; then exit 1; fi
 
 if [ ! -f test.txt ]; then exit 1; fi
 
