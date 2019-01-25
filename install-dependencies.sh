533ffd7741750a2d8778f7e26d7a87ac95b9096a
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 00:14:10 +1300

    oops ... wrong argument!

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,12 +4,12 @@ XQUARTZ_VERSION=2.7.11
 R_VERSION=latest
 
 # install gtk
-curl -LO http://r.research.att.com/libs/GTK_2.24.17-X11.pkg -o gtk.pkg
+curl -L http://r.research.att.com/libs/GTK_2.24.17-X11.pkg -o gtk.pkg
 # sudo installer -pkg gtk.pkg -target / -allowUntrusted
 
 # install xquartz
-curl -LO https://dl.bintray.com/xquartz/downloads/XQuartz-$XQUARTZ_VERSION.dmg -o xquartz.dmg
-
+curl -L https://dl.bintray.com/xquartz/downloads/XQuartz-$XQUARTZ_VERSION.dmg -o xquartz.dmg
+# hdiutil attach xquartz.dmg
 
 
 # install r
