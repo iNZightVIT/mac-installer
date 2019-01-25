65a9bcf939eb0b4dbd7c8fd709bce5c684d0be39
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 10:19:44 +1300

    oops ... no longer want to test for gtk/xquartz

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ev
 
-if [ ! -d /Library/Frameworks/GTK+.framework  ]; then exit 1; fi
-if [ ! -d /opt/X11 ]; then exit 1; fi
+# if [ ! -d /Library/Frameworks/GTK+.framework  ]; then exit 1; fi
+# if [ ! -d /opt/X11 ]; then exit 1; fi
 if [ ! -d /Library/Frameworks/R.framework  ]; then exit 1; fi
 
 ## finally, build the installer - if this is successful, then we have won! 
