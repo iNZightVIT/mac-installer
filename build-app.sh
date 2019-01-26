aa409de1786c37a4f1fa2577aa4680acaba80915
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 17:59:27 +1300

    replace tests with build

--- build-app.sh
+++ build-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ev
 
 APP=iNZightVIT
 INZ=$APP/iNZight/Contents
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,5 +6,10 @@ R_VERSION=latest
 # install r
 curl -L http://cran.stat.auckland.ac.nz/bin/macosx/el-capitan/base/R-$R_VERSION.pkg -o r.pkg
 sudo installer -pkg r.pkg -target / -allowUntrusted
+if [ ! -d /Library/Frameworks/R.framework  ]; then exit 1; fi
 
+# install r packages
 /usr/local/bin/R --slave -f install_inzight.R
+if [ ! -d iNZightVIT/.library ]; then exit 1; fi
+usr/local/bin/R --slave -e ".libPaths('iNZightVIT/.library'); requireNamespace('RGtk2')"
+
