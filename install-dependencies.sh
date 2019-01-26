6fe10d30c88a14bf48ac1a8f3b057909253b86bd
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 18:06:27 +1300

    missing slash

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -11,5 +11,5 @@ if [ ! -d /Library/Frameworks/R.framework  ]; then exit 1; fi
 # install r packages
 /usr/local/bin/R --slave -f install_inzight.R
 if [ ! -d iNZightVIT/.library ]; then exit 1; fi
-usr/local/bin/R --slave -e ".libPaths('iNZightVIT/.library'); requireNamespace('RGtk2')"
+/usr/local/bin/R --slave -e ".libPaths('iNZightVIT/.library'); requireNamespace('RGtk2')"
 
