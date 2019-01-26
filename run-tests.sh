64c358bef97b19f87ba356ff61739923f32f0eb7
Tom Elliott <tomelliottnz@gmail.com>
2019-01-26 17:42:20 +1300

    library renamed to .library

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@ if [ ! -d /Library/Frameworks/R.framework  ]; then exit 1; fi
 ## finally, build the installer - if this is successful, then we have won! 
 # - install these into a separate directory which can then be packaged ... 
 
-ls -al iNZightVIT/library
-R --slave -e ".libPaths('iNZightVIT/library'); requireNamespace('RGtk2')"
+ls -al iNZightVIT/.library
+R --slave -e ".libPaths('iNZightVIT/.library'); requireNamespace('RGtk2')"
 
 exit 0
